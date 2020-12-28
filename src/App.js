cd6a538d669a2d1eb9d9132e7471eb9ea81fffa0
Leah Scott-Zechlin <scottzec@gmail.com>
2020-12-27 21:23:44 -0800

    implemented TimelineEvent component function, hardcoded data working except Timestamp yields a few seconds ago

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import timelineData from './data/timeline.json';
 import Timeline from './components/Timeline';
+import TimelineEvent from './components/TimelineEvent';
 
 function App() {
   console.log(timelineData);
@@ -10,9 +11,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="App-title">Application title</h1>
+        <h1 className="App-title">Ada Lovelace's Social Media Feed</h1>
       </header>
       <main className="App-main">
+        <TimelineEvent person="Nica Cordeiro" status="Fabulous" timestamp="2018-04-13T11:11:03Z" />
       </main>
     </div>
   );
--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,9 +2,19 @@ import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-const TimelineEvent = () => {
+const TimelineEvent = (props) => {
   
-  return;
+  return(
+    <div>
+      <ul className='TimelineEvent'>
+        <li className='EventPoster'> {props.person} </li>
+        <li className='StatusMessage'> {props.status} </li>
+        <li className='EventTimestamp'> <Timestamp time={props.timeStamp}/> </li>
+      </ul>
+    </div>
+  );
 }
 
 export default TimelineEvent;
+
+
